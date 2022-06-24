326d99ec851e82fd5b78b9afcd80c73dc5ed020c
francisGolden <doria5@live.it>
2022-06-24 20:17:39 +0200

    toast

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,13 @@
 import { useContext } from "react";
 import CartContext from "./context/Context";
-import uniqid from "uniqid"
+import uniqid from "uniqid";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Shop = () => {
     const {products, setProducts} = useContext(CartContext)
+
+    const notify = () => toast("Item added to the cart!");
 
     const handleClick = (e) => {
 
@@ -11,14 +15,24 @@ const Shop = () => {
         setProducts(()=>products.map(item=>item.name == e.target.id ? {
             ...item, orders: item.orders + 1}:item))
 
+        
+        notify()
+
     }
-
-
 
     console.log(products)
     
     return (
         <div id="shop-container">
+
+            <ToastContainer 
+                position="top-center"
+                autoClose={200}
+                hideProgressBar={true}
+                theme="colored"
+                
+            />
+
             <div id="shop-grid">
             {
                 products.map((item)=>{
